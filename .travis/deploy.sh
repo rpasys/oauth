ca29dadae5fe7bd01c1cc1538a754b542e2e3e97
Denys Golubiev <denys.golubiev@adorsys.de>
2016-10-04 17:51:43 +0200

    Output decrypted env vars into script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,16 @@
-if [[ "$TRAVIS_TAG" =~ ^oauth-parent-[[:digit:].]+$ ]] then
-  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central"
-  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly
+#!/usr/bin/env bash
+set -ev
+
+echo "GPG_OWNERTRUST: $GPG_OWNERTRUST";
+echo "GPG_SECRET_KEY: $GPG_SECRET_KEY";
+echo "GPG_EXECUTABLE: $GPG_EXECUTABLE";
+
+if [[ "$TRAVIS_TAG" =~ ^oauth-parent-[[:digit:].]+$ ]]; then
+  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
+  #mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly;
 else
-  echo "NO RELEASE TAG -> don't publish to mvn central"
-  mvn package -U -Pwildfly
+  echo "NO RELEASE TAG -> don't publish to mvn central";
+  mvn package -U -Pwildfly;
 fi
 
 
