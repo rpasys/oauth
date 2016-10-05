6f8b2f900aaa5ba714764047be43c2ea33ad8353
Denys Golubiev <denys.golubiev@adorsys.de>
2016-10-05 13:08:18 +0200

    Force add TRAVIS_TAG variable to test deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-echo "GPG_OWNERTRUST: $GPG_OWNERTRUST";
-echo "GPG_SECRET_KEY: $GPG_SECRET_KEY";
-echo "GPG_EXECUTABLE: $GPG_EXECUTABLE";
-
-if [[ "$TRAVIS_TAG" =~ ^oauth-parent-[[:digit:].]+$ ]]; then
+if [[ "$TRAVIS_TAG" =~ "^oauth-parent-[[:digit:].]+$" ]]; then
   echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
   #mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly;
 else
