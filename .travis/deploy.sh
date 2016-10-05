e0be86e2fa1f58bb0779200ad7e59d147f56c827
Denys Golubiev <denys.golubiev@adorsys.de>
2016-10-05 14:03:12 +0200

    cleanup temorary debug settings and variables

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "TRAVIS_TAG: $TRAVIS_TAG"
-
 TAG_PATTERN="^oauth-parent-([[:digit:]]+\.)+[[:digit:]]+$"
 
 if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
   echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
-  #mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly;
+  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly;
 else
   echo "NO RELEASE TAG -> don't publish to mvn central";
-  #mvn package -U -Pwildfly;
 fi
 
 
