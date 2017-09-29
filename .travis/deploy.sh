5a5cda97d87ddb9fcd16c6dd5343a61dcdfd02ae
Denys Golubiev <dgo@adorsys.de>
2017-09-29 16:37:59 +0200

    fixed errors of automatic oss-nexus deployment config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ TAG_PATTERN="^oauth-parent-([[:digit:]]+\.)+[[:digit:]]+$"
 
 if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
   echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
-  mvn deploy javadoc:javadoc gpg:sign -Prelease -DskipTests -B -U -Pwildfly;
+  mvn --settings .travis/settings.xml deploy -Prelease -DskipTests -B -U -Pwildfly;
 else
   echo "NO RELEASE TAG -> don't publish to mvn central";
 fi
